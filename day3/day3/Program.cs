15eb6e10ee16e020d52cc6ee6f54bfe9731a8833
Meghal2103 <meghal2103@outlook.com>
2025-02-10 13:49:23 +0530

    day3

---
+++ day3/day3/Program.cs
@@ -0,0 +1,15 @@
+﻿using day3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee();
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee();
+        Intern intern = new Intern();
+
+        Console.WriteLine(fullTimeEmployee.CalculateSalary(50));
+        Console.WriteLine(partTimeEmployee.CalculateSalary(50));
+        Console.WriteLine(intern.CalculateSalary(50));
+    }
+}
---
+++ day3/day3/day3.cs
@@ -0,0 +1,32 @@
+﻿namespace day3
+{
+    abstract class Employee
+    {
+        public abstract double CalculateSalary(double hours);
+    }
+
+    // Concrete classes for different employee types
+    class FullTimeEmployee : Employee
+    {
+        public override double CalculateSalary(double hours)
+        {
+            return hours * 50;
+        }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public override double CalculateSalary(double hours)
+        {
+            return hours * 30;
+        }
+    }
+
+    class Intern : Employee
+    {
+        public override double CalculateSalary(double hours)
+        {
+            return hours * 10;
+        }
+    }
+}
